d23ccbec93e20ddfdda1763100129aa20160d596
rochamanuela <rochadesouzamanuela@gmail.com>
2023-11-20 14:38:27 -0200

    20/11/2023 - 14:38

--- html/script.js
+++
@@ -1,7 +0,0 @@
-const burgerMenu = document.querySelector('.burger-menu');
-const navLinks = document.querySelector('.nav-links');
-const content = document.querySelector('.content');
-
-burgerMenu.addEventListener('click', () => {
-  document.body.classList.toggle('show-nav');
-});
